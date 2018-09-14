8447d0ad6f03bc15a4718a78fe54a9f6467f162a
duguangting <dgt_x@foxmail.com>
2018-09-14 16:00:13 +0800

    sougou interview problem dp

--- others/sougou.py
+++ others/sougou.py
@@ -18,6 +18,28 @@ def sougou_interview(a, b):
     return count
 
 
+def sougou_dynamic(a, b):
+    import sys
+    nums = [1, 3, 4]
+    dp = {
+        1: 1,
+        3: 1,
+        4: 1
+    }
+    diff = abs(b-a)
+    for i in range(2, diff+1):
+        if i in dp:
+            continue
+        res = sys.maxsize
+        for num in nums:
+            if i - num > 0:
+                res = min(res, dp[i-num])
+        dp[i] = res + 1
+    return dp[diff]
+
+
 if __name__ == '__main__':
-    res = sougou_interview(1, 6)
+    res = sougou_dynamic(-1, -11)
+    print(res)
+    res = sougou_interview(1, 11)
     print(res)
