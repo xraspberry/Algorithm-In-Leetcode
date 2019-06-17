4c7360b2a3bcddf9b16cebc7fba4fc139a8fba1d
duguangting <dgt_x@foxmail.com>
2019-06-17 18:34:30 +0800

    ruby leetcode 20190617

--- Ruby/flipping-an-image.rb
+++ Ruby/flipping-an-image.rb
@@ -1,1 +1,17 @@
-
+# @param {Integer[][]} a
+# @return {Integer[][]}
+def flip_and_invert_image(a)
+  row_len = a.size
+  col_len = a[0].size
+  i = 0
+  while i < row_len
+    a[i].reverse!
+    j = 0
+    while j < col_len
+      a[i][j] = a[i][j] == 1 ? 0 : 1
+      j += 1
+    end
+    i += 1
+  end
+  return a
+end
